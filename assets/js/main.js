db116b6e25192ab63cb5e53b3bdafd43ddeb2ce5
Francisco Peña Villalobos <168454078+Fcojpv@users.noreply.github.com>
2024-11-14 09:37:31 -0300

    correccion de indentacion y css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,11 @@
+
+$(document).ready(function() {
+
+$("#TituloQuienesSomos").on("click", function() {
+  alert("Bienvenidos a Viajes Chile"); 
+});
+});
+
 /* Tool tip  */
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-// 
-$(document).ready(function() {
-
-// evento "Quienes Somos"
-$("#TituloQuienesSomos").on("click", function() {
-  alert("Hola ! Bienvenid@ a Viajes Chile !");
-});
-
-  });
